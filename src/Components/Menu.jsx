41ceee9bf0533d6d3c1dc2d130e5ccd707d7cf38
Huxyshuu <hugotamm@hotmail.com>
2022-06-18 15:40:50 +0300

    Added a menu button and other stuff

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -6,18 +6,40 @@ export default function Menu() {
 
     const [menuOpen, setMenuOpen] = useState(false);
 
-    useEffect(() => {
 
-    })
+    /* DISABLES SCROLLING */
+    useEffect(() => {
+        if (menuOpen) {
+            document.body.style.overflow = "hidden";
+        } else {
+            document.body.style.overflow = "visible";
+        }
+    }, [menuOpen])
 
     return (
-        <div id="menu">
+        <div>
             { !menuOpen && <Icon icon="ci:menu-alt-01" id="closedMenuButton" onClick={() => setMenuOpen(true)}/> }
             { menuOpen && 
             <>
-                <Icon icon="eva:close-circle-outline" id="openedMenuButton" onClick={() => setMenuOpen(false)}/>
-                <div>
-                    
+                <div id="menuBox">
+                    <div id="menuTitle">
+                        <Icon icon="eva:close-circle-outline" id="openedMenuButton" onClick={() => setMenuOpen(false)}/>
+                        <div id="logo">
+                            <img src={require('../images/ht-logo-yellow.png')} alt="" />
+                            <h2>Hugo</h2>
+                            <p>Developer</p>
+                        </div>
+                        
+                    </div>
+                    <div id="menuItems">
+                        <p id="menuWork" className="menuItems">Work</p>
+                        <p id="menuAbout" className="menuItems">About</p>
+                        <p id="menuContact" className="menuItems">Contact</p>
+                    </div>
+                    <div id="contactIcons">
+                        <Icon icon="cib:linkedin" />
+                        <Icon icon="carbon:logo-github" />
+                    </div>
                 </div>
             </>
             }
